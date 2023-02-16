ddb2f72ca8d6227b9bfac26dc23a1a59b157d818
Tratatatov <tratatatov@gmail.com>
2023-02-16 20:16:03 +0300

    First

---
+++ Assets/AwakeTest.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class AwakeTest : MonoBehaviour
+{
+
+    void Awake()
+    {
+        Debug.Log("Awakes");
+    }
+    void Start()
+    {
+        Debug.Log("Starts");
+    }
+
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefab;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(prefab);
+        }
+    }
+}
